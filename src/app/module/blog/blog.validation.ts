5b01396ea0932deba71112fe7485fbcc27717a78
SumonRahman24 <mustafizurrahmansumon24@gmail.com>
2024-12-31 05:34:00 +0600

    connection with mongodb successfully

--- src/app/module/blog/blog.validation.ts
+++ src/app/module/blog/blog.validation.ts
@@ -2,31 +2,36 @@ import { z } from "zod";
 import { ObjectId } from "mongodb";
 
 export const CreateBlogValidationSchema = z.object({
-  title: z
-    .string()
-    .max(255, "Title must not exceed 255 characters")
-    .nonempty("Title is required"),
-  content: z
-    .string()
-    .min(10, "Content must be at least 10 characters long")
-    .nonempty("Content is required"),
-  authorId: z.instanceof(ObjectId).refine((id) => id instanceof ObjectId, {
-    message: "Invalid author ID",
+  body: z.object({
+    title: z
+      .string()
+      .max(255, "Title must not exceed 255 characters")
+      .nonempty("Title is required"),
+    content: z
+      .string()
+      .min(10, "Content must be at least 10 characters long")
+      .nonempty("Content is required"),
+    authorId: z.string(),
   }),
 });
 
 export const UpdateBlogValidationSchema = z.object({
-  title: z.string().max(255, "Title must not exceed 255 characters").optional(),
-  content: z
-    .string()
-    .min(10, "Content must be at least 10 characters long")
-    .optional(),
-  authorId: z
-    .instanceof(ObjectId)
-    .refine((id) => id instanceof ObjectId, {
-      message: "Invalid author ID",
-    })
-    .optional(),
+  body: z.object({
+    title: z
+      .string()
+      .max(255, "Title must not exceed 255 characters")
+      .optional(),
+    content: z
+      .string()
+      .min(10, "Content must be at least 10 characters long")
+      .optional(),
+    authorId: z
+      .instanceof(ObjectId)
+      .refine((id) => id instanceof ObjectId, {
+        message: "Invalid author ID",
+      })
+      .optional(),
+  }),
 });
 
 export const blogSchemaValidation = {
